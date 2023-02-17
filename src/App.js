8b4919119d35bf19d43fe8db663dfd424196e37c
0xn4utilus <gyanendrabanjare8@gmail.com>
2023-02-17 19:58:14 +0530

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home/Index';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Home/>
     </div>
   );
