a1feb0ee92f7607b82fcf5639008b7b51996d765
manasghandat <ghandatmanas@gmail.com>
2023-02-17 19:09:39 +0530

    Added sidebar

---
+++ src/Components/Sidebar/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import SettingsIcon from '@mui/icons-material/Settings';
+import HomeIcon from '@mui/icons-material/Home';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import HistoryIcon from '@mui/icons-material/History';
+import LocalFireDepartmentSharpIcon from '@mui/icons-material/LocalFireDepartmentSharp';
+
+export default function Sidebar(){
+    const [enable_sidebar,Set_enable_sidebar] = React.useState(false);
+    return(
+        <div>
+            <Button onClick={()=>{Set_enable_sidebar(true)}}>Toggle</Button>
+
+            <Drawer anchor='left' open={enable_sidebar} onClose={()=>{Set_enable_sidebar(false)}}>
+                <Box sx={{width:250}}>
+                    <List >
+                        <ListItem disablePadding  >
+                            <ListItemButton>
+                            <ListItemIcon>
+                                <HomeIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Home" />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem disablePadding >
+                            <ListItemButton>
+                            <ListItemIcon>
+                                <LocalFireDepartmentSharpIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Trending" />
+                            </ListItemButton>
+                        </ListItem>
+                        <Divider />
+                        <ListItem disablePadding >
+                            <ListItemButton>
+                            <ListItemIcon>
+                                <VideoLibraryIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Library" />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem disablePadding >
+                            <ListItemButton>
+                            <ListItemIcon>
+                                <HistoryIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="History" />
+                            </ListItemButton>
+                        </ListItem>
+                        <Divider/>
+                        <ListItem disablePadding >
+                            <ListItemButton>
+                            <ListItemIcon>
+                                <SettingsIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary="Settings" />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                </Box>
+            </Drawer>
+        </div>
+    )
+}
