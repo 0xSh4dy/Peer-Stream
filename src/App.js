fc2986f2246f1a0c3c4528ba5d729a10e66f5086
0xSh4dy <rakshitawasthi02@gmail.com>
2023-02-17 20:44:44 +0530

    Updated themes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./Components/Home/Index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./Components/Sidebar";
+import LivePeerTest from "./Components/LivePeerTest";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home/>}/>
         <Route path="/" element={<Sidebar/>}/>
+        <Route path="/testing" element={<LivePeerTest/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/Components/LivePeerTest.js
+++ src/Components/LivePeerTest.js
@@ -17,8 +17,8 @@ export default function LivePeerTest() {
   return (
     <div>
       <LivepeerConfig client={reactClient}>
-        {/* <CreateAndViewAsset/> */}
-        <NewStream/>
+        <CreateAndViewAsset/>
+        {/* <NewStream/> */}
       </LivepeerConfig>
     </div>
   );
