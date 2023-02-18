14754f4e7d6a735334fffd5dc17f17c3b639c5d0
manasghandat <ghandatmanas@gmail.com>
2023-02-18 16:28:07 +0530

    Fixed sidebar final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./Components/Home/Index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/home" element={<Home/>}/>
+        <Route path="/" element={<Sidebar/>}/>
       </Routes>
     </BrowserRouter>
   );
